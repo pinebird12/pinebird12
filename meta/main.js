e9a2439f8131df79658e1f25596c5f129b617ac9
Martin Hawks <martinhawks8@gmail.com>
2025-05-09 10:42:49 -0700

    Made a nice plot

--- meta/main.js
+++ meta/main.js
@@ -1,5 +1,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
+
+// Functions and processing code to be used later
 async function loadData() {
   const data = await d3.csv('loc.csv', (row) => ({
     ...row,
@@ -9,8 +11,188 @@ async function loadData() {
     date: new Date(row.date + 'T00:00' + row.timezone),
     datetime: new Date(row.datetime),
   }));
-  console.log(data);
   return data;
 }
 
+
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let out = {
+        id: commit,
+        url: 'https://github.com/mawks12/106l1/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        // Calculate hour as a decimal for time analysis
+        // e.g., 2:30 PM = 14.5
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        // How many lines were modified?
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(out, 'lines', {
+        value: lines,
+        enumerable: false,
+      });
+
+      return out;
+    });
+}
+
+function renderCommitInfo(data, commits) {
+  // Create the dl element
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  // Add total LOC
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  // Add total commits
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  dl.append('dt').text('Average Line Length');
+  dl.append('dd').text(d3.mean(commits, (d) => d.lines.length));
+
+  dl.append('dt').text('Average # files changed')
+  dl.append('dd').text(d3.mean(commits, (d) => (
+    d3.count(d.lines, (e) => e.file)
+  )));
+
+  dl.append('dt').text('Lines Changed in last commit');
+  dl.append('dd').text(commits[0].lines.length);
+}
+
+
+function renderScatterPlot(data, commits) {
+  // Sizes of plot, and spacing
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+
+  // create plot svg
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  // define scales for axis
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
+
+  // Add gridlines
+   const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
+
+  // Create gridlines as an axis with no labels and full-width ticks
+  gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+
+  // Create the axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+  // Add X axis
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  // Add Y axis
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
+
+  // add datapoints
+  const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
+
+  const dots = svg.append('g').attr('class', 'dots');
+  const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+  const rScale = d3
+    .scaleSqrt() // Change only this line
+    .domain([minLines, maxLines])
+    .range([2, 30]);
+
+  dots
+    .selectAll('circle')
+    .data(sortedCommits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', (d) => rScale(d.totalLines))
+    .style('fill-opacity', 0.7)
+    .attr('fill', 'steelblue')
+    .on('mouseenter', (event, commit) => {
+      d3.select(event.currentTarget).style('fill-opacity', 1); // Full opacity on hover
+      d3.select("#commit-tooltip").style('visibility', 'visible')
+      renderTooltipContent(commit);
+      updateTooltipPosition(event);
+    })
+    .on('mouseleave', () => {
+      d3.select(event.currentTarget).style('fill-opacity', 0.7);
+      d3.select("#commit-tooltip").style('visibility', 'hidden')
+    });
+}
+
+function renderTooltipContent(commit) {
+  const link = document.getElementById('commit-link');
+  const date = document.getElementById('commit-date');
+
+  if (Object.keys(commit).length === 0) return;
+
+  link.href = commit.url;
+  link.textContent = commit.id;
+  date.textContent = commit.datetime?.toLocaleString('en', {
+    dateStyle: 'full',
+  });
+}
+
+function updateTooltipPosition(event) {
+  const tooltip = document.getElementById('commit-tooltip');
+  tooltip.style.left = `${event.clientX}px`;
+  tooltip.style.top = `${event.clientY}px`;
+}
+
+
+
+
+
+
+// initilize variables
 let data = await loadData();
+let commits = processCommits(data);
+
+console.log(data);
+console.log(commits);
+
+renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);

4a76f0cdb1463e23e1ba3c31bc3387a3c2d3a605
Ivy Hawks <martinhawks8@gmail.com>
2026-08-25 17:45:42 -0700

    bug fix

--- global.js
+++ global.js
@@ -146,7 +146,7 @@ export function renderProjects(projects, container) {
 
         if (!(project['image'] === null)) {
           const image = document.createElement('img');
-          image.setAttribute('src', project['image']);
+          image.setAttribute('src', `./projects/${project['image']}`);
           article.appendChild(image);
         }
         article.appendChild(desc);
